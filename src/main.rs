e3bd87295e93dcf2d361606be9864dcf3dcf66fa
pnhearer <pnhearer@gmail.com>
2023-10-19 07:39:37 -0400

    Back to the basics. Deleted the Base connection and the network device files. We are focusing on the primary complexity to building our devices in our scope.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,67 @@
-use std::io::*;
+use std::io::{Read, Write};
+use std::net::{IpAddr, TcpStream};
+use ssh2::*;
+//use std::io::prelude::*;
+use std::time::Duration;
 
-#[allow(dead_code)]
-use crate::base_connection::base_connection::BaseConnection;
-
-mod base_connection;
-mod network_device;
 
 fn main() {
-    let device = BaseConnection::new();
-    if device.stream.authenticated() {
-        println!("Session authenticated");
-    } else {
-        println!("Session not authenticated");
+    let ipv4 = std::net::SocketAddr::new(IpAddr::V4("127.0.0.1".parse().unwrap()), 22);
+    let tcp = match TcpStream::connect_timeout(&ipv4, Duration::new(5, 0)) {
+        Ok(tcp) => tcp,
+        _ => return
+    };
+    // Begin Session build
+    let mut session = match Session::new() {
+        Ok(session) => session,
+        _ => return
+    };
+    // Takes ownership of TCP stream
+    session.set_tcp_stream(tcp);
+    // Handshake transport protocol
+    match session.handshake() {
+        Ok(()) => (),
+        Err(e) => println!("Error: {}", e)
+    };
+
+    // Authentication
+    match session.userauth_password("username", "password") {
+        Ok(()) => (),
+        Err(e) => println!("Error: {}", e)
+    };
+    // Create channel, be lazy and use expect.
+    let mut channel = session.channel_session().expect("Failed to establish channel");
+    // Ask for a Shell
+    match channel.shell() {
+        Ok(()) => (),
+        Err(e) => println!("{}", e)
     }
-    let mut channel = device.stream.channel_session().unwrap();
-    channel.exec("pwd").unwrap();
-    let mut return_from_shell = String::new();
-    channel.read_to_string(&mut return_from_shell).unwrap();
-    println!("{}", return_from_shell);
+
+    //Let's Issue commands to the remote host!
+    channel.write_all(b"ls").unwrap();
+
+    //Begin the read loop
+
+    loop {
+        let mut rbuff = [0u8;1024];
+        match channel.read(&mut rbuff) {
+            Ok(0) => break,
+            Ok(c) => {
+                let slice = &rbuff[0..c];
+                match std::str::from_utf8(slice) {
+                    Ok(s) => print!("{}",s ),
+                    Err(e) => {
+                        eprint!("Output was not UTF-8. Instead was {}", e);
+                        break
+                    }
+                }
+            },
+            Err(e) => {
+                println!("Error while reading {}",e );
+                break
+            }
+        }
+        };
 }
+
+
